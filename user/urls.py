df8eab1a3ff7b67b85b9c26fe4e3bdc5862a761b
taein Kim <kti0940@gmail.com>
2022-06-29 15:14:25 +0900

    feat - JWT인증토큰 발급 및 로그인 완료

--- user/urls.py
+++ user/urls.py
@@ -5,10 +5,13 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView, # 기본 JWT access토큰 발급 view
     TokenRefreshView) # JWT Refresh 토큰 발급 view
 
+from user.views import YujeonTokenObtainPairView
+
 urlpatterns = [
     path('', views.UserView.as_view()),
     path('login/', views.UserAPIView.as_view()),
     path('logout/', views.UserAPIView.as_view()),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/yujeon/token/', YujeonTokenObtainPairView.as_view())
 ]
