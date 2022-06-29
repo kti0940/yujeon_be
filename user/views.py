4b916100a55a6b2d13bc996a992335ed7818235c
taein Kim <kti0940@gmail.com>
2022-06-29 11:45:02 +0900

    feat - 회원가입, 로그아웃 완료 및 로그인 진행중

--- user/views.py
+++ user/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth import login, logout, authenticate
+from user.serializers import UserSignUpSerializer
 
 # Create your views here.
+
+class UserView(APIView):
+    def get(self, request):
+        pass
+    
+    # 회원가입 
+    def post(self, request):
+        serializer = UserSignUpSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message":"회원가입에 성공했습니다!"})
+        else:
+            print(serializer.errors)
+            return Response({"message": "회원가입 실패"})
+
+    def put(self, request):
+        pass
+
+    def delete(self, request):
+        pass
+
+class UserAPIView(APIView):
+    
+    # 로그인
+    def post(self, request):
+        username = request.data.get("username",'')
+        password = request.data.get("password",'')
+
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        
+        if not user:
+            return Response({"error": "존재하지 않는 계정이거나 패스워드가 일치하지 않습니다"})
+        
+        
+        login(request, user)  
+        
+        return Response({"message": "login success!!"})
+    # 로그아웃
+    def delete(self, request):
+        logout(request)
+        return Response({"로그아웃 성공 안녕히가세요"})
