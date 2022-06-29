07c7b9f5adabb35329a9edd0383604cd345cde49
kimheejeong3808 <khjhj3808@gmail.com>
2022-06-29 16:19:21 +0900

    feat-post-upload

--- post/models.py
+++ post/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.forms import DateTimeField
 
 # Create your models here.
+class Post(models.Model):
+    artist = models.ForeignKey('user.User',verbose_name="작성자", on_delete=models.CASCADE)
+    title = models.CharField("제목", max_length=50)
+    image = models.ImageField(upload_to='')
+    desc = models.TextField("작품설명")
+    cost = models.IntegerField("가격")
+    is_mine = models.BooleanField(default=True)
+    created_at = models.DateTimeField("작성일", auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
--- post/views.py
+++ post/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from post.serializers import PostSerializer
+from .models import Post as PostModel
+
 
 # Create your views here.
+class PostView(APIView):
+    # 포스트 조회
+    
+    # 포스트 업로드
+    def post(self, request):
+        request.data['artist'] = request.user.id
+        post_serializer = PostSerializer(data=request.data)
+
+        if post_serializer.is_valid():
+            post_serializer.save()
+            return Response(post_serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(post_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    # 포스트 수정
+    def put(self, request, post_id):
+        post = PostModel.objects.get(id=post_id)
+        post_serializer = PostSerializer(post, data=request.data, partial=True)
+        
+        if post_serializer.is_valid():
+            post_serializer.save()
+            return Response(post_serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(post_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    # 포스트 삭제
+    def delete(self, request):
+        return Response({'message': '삭제 성공!'})
